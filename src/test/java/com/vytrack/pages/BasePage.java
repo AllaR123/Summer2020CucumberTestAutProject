2807fcf45c8268fb25d27e6cbb87ef34ca10f0cb
Alla Rudenko <whitney2206@gmail.com>
2020-12-15 19:44:00 -0500

    new

--- src/test/java/com/vytrack/pages/BasePage.java
+++ src/test/java/com/vytrack/pages/BasePage.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-// parent class for all classes. Used to initialize page factory (for @FindBy)
+// parent class for all classes. Used to initialize all the objects using Page Factory (for @FindBy)
 public abstract class BasePage {                // cannot create obj from this class
 
     @FindBy(className = "oro-subtitle")
