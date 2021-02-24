48ebd0aa9d3ee2342ef2a1da518fe82a8bd7f275
Alla Rudenko <whitney2206@gmail.com>
2021-02-23 20:14:08 -0500

    new

--- src/test/java/com/vytrack/step_definitions/Hooks.java
+++ src/test/java/com/vytrack/step_definitions/Hooks.java
@@ -4,6 +4,8 @@ import com.vytrack.utils.Driver;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
 import java.util.concurrent.TimeUnit;
 
@@ -21,41 +23,40 @@ public class Hooks {
 //    hook after = @AfterMethod in TestNG
 //    it's not a good idea to mix implicit and explicit waits. It can lead to unexpectedly long waits.
 //    usually, we just use explicit and fluent waits.
-
-        @Before
-        public void setup(Scenario scenario) {
-            // in this line we specify what scenarios we were running, change the browser on the way as well!
-            // [@negative_login]
-            System.out.println(scenario.getSourceTagNames());
-            System.out.println("::: Starting Automation:::");
-            Driver.getDriver().manage().window().maximize();
-            Driver.getDriver().manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-        }
-
-        @After
-        public void tearDown() {
-            //close browser, close DB connection, close tunnel,capture screenshot of the error, etc..
-            //this is a hook after
-            //runs automatically after every test
-            Driver.closeDriver();
-            System.out.println(":::(^_^) End of test execution (*_*):::");
-        }
-
-    //    this hook will run only before scenarios with a tag @db
-
+    @Before
+    public void setup(Scenario scenario) {
+        System.out.println("::: Starting Automation:::");
+        Driver.getDriver().manage().window().maximize();
+        Driver.getDriver().manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+    }
+//    this hook will run only before scenarios with a tag @db
     /**
-     * @db
-     * Scenario: I don't know but here I'm connecting to DB
+     * @db Scenario: I don't know but here I'm connecting to DB
      * Given user runs following query "SELECT * ...."
+     * <p>
+     * order = 0 - to define hooks execution order
      */
-    @Before("@db")
-    public void dbSetup(){
+    @Before(value = "@db", order = 0)
+    public void dbSetup() {
         System.out.println("::: Connecting to the database:::");
     }
-
     @After("@db")
-    public void dbTearDown(){
+    public void dbTearDown() {
         System.out.println("::: Disconnecting from the database:::");
+    }
+    @After
+    public void tearDown(Scenario scenario) {
+        //close browser, close DB connection, close tunnel,capture screenshot of the error, etc..
+        //this is a hook after
+        //runs automatically after every test
+        if (scenario.isFailed()) {
+            byte[] data = ((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+            // adding the screenshot to the report
+            scenario.attach(data, "image/png", scenario.getName());
+        }
+        Driver.closeDriver();
+        System.out.println(":::(^_^) End of test execution (*_*):::");
+
     }
 
     }
